057143ef26885658a507e1d1c9f6e45a8babbd37
Gyanendra Shukla <srirajshukla@gmail.com>
2021-11-15 23:17:58 +0530

    feat: add write macro and formatted string

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,5 @@
 use volatile::Volatile;
+use core::fmt::{self, Write};
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -91,6 +92,13 @@ impl Writer {
     }
 }
 
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_something(){
     let mut writer = Writer {
         column_position: 12, 
@@ -101,4 +109,5 @@ pub fn print_something(){
     writer.write_byte(b'H');
     writer.write_string("ello, ");
     writer.write_string("w⍺rld!");
+    write!(writer, "a = {}, a/b = {}", 3, 1.0/3.0).unwrap();
 }
